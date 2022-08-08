8bb380d896cd30f1ef74c061214c3a2ed8299096
Yuriy Pishchenko <locbne@gmail.com>
2022-08-08 11:34:28 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__text">© {new Date().getFullYear()} Mesto Russia</p>
-    </footer>
-  );
-}
-
-export default Footer;
